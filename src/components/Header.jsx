7730bfdc871e08d673958fabcde52d8f41cd7f15
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-04 21:51:19 +0500

    added App jsx, styling form application, made components folder and removed unnecessary styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="src/assets/react.svg" alt="React logo" />
+        <span> React Facts </span>
+      </nav>
+    </header>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import MainContent from './MainContent.jsx'
-import Footer from './Footer.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode><App /></StrictMode>
-// )
 
 const root = createRoot(document.getElementById('root'))
 
-function Page() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  )
-}
-
 root.render(
-    <Page />
+    <App />
 )
