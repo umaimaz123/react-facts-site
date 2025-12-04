6737ff49d9fd98bba8f04159f8740d2a2ab275ea
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-04 19:51:45 +0500

    using createRoot to render html elements

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode><App /></StrictMode>
+// )
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <div>
+      <header>
+        <img src="src/assets/react.svg" width="40px" alt="React logo" />
+      </header>
+      <main>
+        <h1> Fun Facts about React!</h1>
+          <ul>
+            <li> Was first released in 2013 </li>
+            <li> Was originally created by Jordan Walke </li>
+            <li> Has well over 200K stars on GitHub </li>
+            <li> Is maintained by Meta </li>
+            <li> Powers thousands of enterprise apps, including mobile apps </li>
+            <li> Was first released in 2013 </li>
+          </ul>
+      </main>
+      <footer>
+        © 2025 Fun Facts About React. All rights reserved.
+      </footer>
+    </div>
 )
