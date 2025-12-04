565ca70841152023acb4b5e7c832ee225d7c5c2a
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-04 19:51:45 +0500

    added styling in jsx file using className

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,15 @@ const root = createRoot(document.getElementById('root'))
 
 function Header() {
   return (
-    <header>
-      <img src="src/assets/react.svg" width="40px" alt="React logo" />
+    <header className="header">
+      <img src="src/assets/react.svg" className="nav-logo" alt="React logo" />
+      <nav>
+        <ul className='nav-list'>
+          <li> Pricing </li>
+          <li> About </li>
+          <li> Contact </li>
+        </ul>
+      </nav>
     </header>
   )
 }
