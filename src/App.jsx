ca1e9ee2580b40ef73ad3e7353915d64a351272f
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-05 15:59:01 +0500

    added styling for main content and removed unnecessary styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,15 +1,14 @@
 export default function MainContent() {
   return (
     <main>
-        <h1> Fun Facts about React!</h1>
-          <ul>
-            <li> Was first released in 2013 </li>
-            <li> Was originally created by Jordan Walke </li>
-            <li> Has well over 200K stars on GitHub </li>
-            <li> Is maintained by Meta </li>
-            <li> Powers thousands of enterprise apps, including mobile apps </li>
-            <li> Was first released in 2013 </li>
-          </ul>
-      </main>
+      <h1> Fun Facts about React!</h1>
+        <ul>
+          <li> Was first released in 2013 </li>
+          <li> Was originally created by Jordan Walke </li>
+          <li> Has well over 200K stars on GitHub </li>
+          <li> Is maintained by Meta </li>
+          <li> Powers thousands of enterprise apps, including mobile apps </li>
+        </ul>
+    </main>
   )
 }
