b009576b5b32dd3bd5d51dcb0f55869771585bab
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-05 16:14:09 +0500

    added styling for facts listing and removed code footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
-import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
     <>
       <Header />
       <MainContent />
-      <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      © 2025 Fun Facts About React. All rights reserved.
-    </footer>
-  )
-}
